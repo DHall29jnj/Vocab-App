8256bbff292e16f3772388a1864a3ed4b545a48f
DHall29jnj <DHall29@its.jnj.com>
2025-06-25 16:08:17 -0400

    reset complete

---
+++ play.py
@@ -0,0 +1,7 @@
+# 2. create a game, where a user has to get through as many items in the list (i.e. answer them correctly), and when they ultimately fail, then they get a high score and have to start again (which also forces them to continue practicing)
+
+# 2.1 is to create the functions to read both the long list of space-repeated words so that we can loop over them and test the user continuously until they fail, at which point we give them a score, and also the word definitions so that we may show a user a word definition if it's their first time encountering the word
+
+# 2.2 define a continuous while loop, that tests the user on each word in the list, one after another in the provided order, and will only exit when each word has been mastered, or the user makes a mistake
+
+# 2.3 print the new high score when they fail or win, and also a command to start again
